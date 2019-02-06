2b140fc78acae017d38804df3b68ba3938e7f318
anuveyatsu <anuar.ustayev@gmail.com>
2019-02-06 10:58:08 +0600

    [data][m]: use GVA per head instead of total GVA

--- london-gva.py
+++ london-gva.py
@@ -4,31 +4,17 @@ from dataflows import Flow, load, dump_to_path, PackageWrapper, ResourceWrapper,
 
 
 def set_format_and_name(package: PackageWrapper):
-    package.pkg.descriptor['title'] = 'London gva'
-    package.pkg.descriptor['name'] = 'london-gva'
+    package.pkg.descriptor['title'] = 'London Gross Value Added (GVA)'
+    package.pkg.descriptor['name'] = 'gva'
     # Change path and name for the resource:
-
-    package.pkg.descriptor['resources'][0]['path'] = 'data/gva-london.csv'
-    package.pkg.descriptor['resources'][0]['name'] = 'london-gva'
-
-    package.pkg.descriptor['resources'][1]['path'] = 'data/gva.csv'
-    package.pkg.descriptor['resources'][1]['name'] = 'gva'
-
-
+    package.pkg.descriptor['resources'][0]['path'] = 'data/gva.csv'
+    package.pkg.descriptor['resources'][0]['name'] = 'gva'
 
     yield package.pkg
     res_iter = iter(package)
     first: ResourceWrapper = next(res_iter)
-    second: ResourceWrapper = next(res_iter)
     yield first.it
-    yield second.it
     yield from package
-
-
-def filter_gva_london(rows):
-    for row in rows:
-        if row['NUTS code'] == 'UKI':
-            yield row
 
 def filter_gva(rows):
     for row in rows:
@@ -36,38 +22,35 @@ def filter_gva(rows):
             yield row
 
 unpivot_fields = [
-    {'name': '1997', 'keys': {'Value': '1997', 'Year': '1997'}},
-    {'name': '1998', 'keys': {'Value': '1998', 'Year': '1998'}},
-    {'name': '1999', 'keys': {'Value': '1999', 'Year': '1999'}},
-    {'name': '2000', 'keys': {'Value': '2000', 'Year': '2000'}},
-    {'name': '2001', 'keys': {'Value': '2001', 'Year': '2001'}},
-    {'name': '2002', 'keys': {'Value': '2002', 'Year': '2002'}},
-    {'name': '2003', 'keys': {'Value': '2003', 'Year': '2003'}},
-    {'name': '2004', 'keys': {'Value': '2004', 'Year': '2004'}},
-    {'name': '2005', 'keys': {'Value': '2005', 'Year': '2005'}},
-    {'name': '2006', 'keys': {'Value': '2006', 'Year': '2006'}},
-    {'name': '2007', 'keys': {'Value': '2007', 'Year': '2007'}},
-    {'name': '2008', 'keys': {'Value': '2008', 'Year': '2008'}},
-    {'name': '2009', 'keys': {'Value': '2009', 'Year': '2009'}},
-    {'name': '2010', 'keys': {'Value': '2010', 'Year': '2010'}},
-    {'name': '2011', 'keys': {'Value': '2011', 'Year': '2011'}},
-    {'name': '2012', 'keys': {'Value': '2012', 'Year': '2012'}},
-    {'name': '2013', 'keys': {'Value': '2013', 'Year': '2013'}},
-    {'name': '2014', 'keys': {'Value': '2014', 'Year': '2014'}}
+    {'name': '1997', 'keys': {'Year': '1997'}},
+    {'name': '1998', 'keys': {'Year': '1998'}},
+    {'name': '1999', 'keys': {'Year': '1999'}},
+    {'name': '2000', 'keys': {'Year': '2000'}},
+    {'name': '2001', 'keys': {'Year': '2001'}},
+    {'name': '2002', 'keys': {'Year': '2002'}},
+    {'name': '2003', 'keys': {'Year': '2003'}},
+    {'name': '2004', 'keys': {'Year': '2004'}},
+    {'name': '2005', 'keys': {'Year': '2005'}},
+    {'name': '2006', 'keys': {'Year': '2006'}},
+    {'name': '2007', 'keys': {'Year': '2007'}},
+    {'name': '2008', 'keys': {'Year': '2008'}},
+    {'name': '2009', 'keys': {'Year': '2009'}},
+    {'name': '2010', 'keys': {'Year': '2010'}},
+    {'name': '2011', 'keys': {'Year': '2011'}},
+    {'name': '2012', 'keys': {'Year': '2012'}},
+    {'name': '2013', 'keys': {'Year': '2013'}},
+    {'name': '20143', 'keys': {'Year': '2014'}}
 ]
 
 extra_keys = [
-    {'name': 'Year', 'type': 'any'}
+    {'name': 'Year', 'type': 'year'}
 ]
-extra_value = {'name': 'Value', 'type': 'any'}
+extra_value = {'name': 'Value', 'type': 'number'}
 
 def london_gva(link):
     Flow(
         load(link,
-             sheet=2),
-        filter_gva_london,
-        load(link,
-             sheet=2),
+             sheet=3),
         filter_gva,
         unpivot(unpivot_fields, extra_keys, extra_value),
         set_format_and_name,
