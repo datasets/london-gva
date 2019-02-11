4dc333275ba5b18282f604cef4838c30be4bfb38
Stefan Boskovic <stefanbole94@gmail.com>
2019-02-11 12:55:24 +0100

    replaced unpivoting rows with regex

--- london-gva.py
+++ london-gva.py
@@ -29,38 +29,20 @@ def remove_duplicates(rows):
         seen.add(line)
         yield row
 
-unpivot_fields = [
-    {'name': '1997', 'keys': {'Year': '1997-01-01'}},
-    {'name': '1998', 'keys': {'Year': '1998-01-01'}},
-    {'name': '1999', 'keys': {'Year': '1999-01-01'}},
-    {'name': '2000', 'keys': {'Year': '2000-01-01'}},
-    {'name': '2001', 'keys': {'Year': '2001-01-01'}},
-    {'name': '2002', 'keys': {'Year': '2002-01-01'}},
-    {'name': '2003', 'keys': {'Year': '2003-01-01'}},
-    {'name': '2004', 'keys': {'Year': '2004-01-01'}},
-    {'name': '2005', 'keys': {'Year': '2005-01-01'}},
-    {'name': '2006', 'keys': {'Year': '2006-01-01'}},
-    {'name': '2007', 'keys': {'Year': '2007-01-01'}},
-    {'name': '2008', 'keys': {'Year': '2008-01-01'}},
-    {'name': '2009', 'keys': {'Year': '2009-01-01'}},
-    {'name': '2010', 'keys': {'Year': '2010-01-01'}},
-    {'name': '2011', 'keys': {'Year': '2011-01-01'}},
-    {'name': '2012', 'keys': {'Year': '2012-01-01'}},
-    {'name': '2013', 'keys': {'Year': '2013-01-01'}},
-    {'name': '20143', 'keys': {'Year': '2014-01-01'}}
-]
+unpivoting_fields = [{'name': '([0-9]{4,5})', 'keys': {'Year': '01-01-' r'\1'}}]
 
 extra_keys = [
-    {'name': 'Year', 'type': 'date'}
+    {'name': 'Year', 'type': 'any'}
 ]
-extra_value = {'name': 'Value', 'type': 'number'}
+extra_value = {'name': 'Value', 'type': 'any'}
+
 
 def london_gva(link):
     Flow(
         load(link,
              sheet=3),
         filter_gva,
-        unpivot(unpivot_fields, extra_keys, extra_value),
+        unpivot(unpivoting_fields, extra_keys, extra_value),
         remove_duplicates,
         set_format_and_name,
         dump_to_path(),
